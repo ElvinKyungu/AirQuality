fae74b7452343fe9ba2098b2f98deebf8836aa21
ElvinKyungu <elvinkyungu.75@gmail.com>
2023-04-15 15:02:34 +0200

    first commit

---
+++ src/App.vue
@@ -0,0 +1,14 @@
+<script setup>
+import Card from '@/components/Card.vue'
+</script>
+
+<template>
+    <div id="app">
+        <div class="background-layer"></div>
+       <card/>
+    </div>
+</template>
+
+<style scoped>
+
+</style>
---
+++ src/components/Card.vue
@@ -0,0 +1,63 @@
+<script>
+export default {
+   
+  data() {
+    return {
+      airQualityData: [],
+    };
+  },
+  mounted() {
+    fetch('')
+      .then(response => response.json())
+      .then(data => {
+        this.airQualityData.push({
+          city: data.data.city,
+          pm25: data.data.current.pollution.pm25,
+          pm10: data.data.current.pollution.pm10,
+        });
+      })
+      .catch(error => console.error(error));
+  },
+}
+</script>
+<template>
+   <div class="app-container">
+      <div class="card">
+         <h1>Air Quality App</h1>
+         <div class="information-block">
+            <div class="loader active">
+               <img src="./icons/biodegradable.svg" class="loader-logo" alt="loader" srcset="">
+            </div>
+            <img src="./icons/magnifying-glass.svg" alt="feeling image" class="emoji-logo" srcset="">
+            <p class="user-information">Your city information</p>
+            <table>
+               <tr>
+                  <th>City</th>
+                  <td>...</td>
+               </tr>
+               <tr>
+                  <th>Pollution info</th>
+                  <td>...</td>
+               </tr>
+               <tr>
+                  <th>Air Quality</th>
+                  <td>...</td>
+               </tr>
+            </table>
+            <p class="scale-bar-text">Pollution scale :</p>
+            <div class="scale-bar">
+               <img src="./icon/location.svg" alt="" srcset="">
+               <div class="scale-section"></div>
+               <div class="scale-section"></div>
+               <div class="scale-section"></div>
+               <div class="scale-section"></div>
+               <div class="scale-section"></div>
+               <div class="scale-section"></div>
+            </div>
+         </div>
+      </div>
+   </div>
+</template>
+<style scoped>
+
+</style>
